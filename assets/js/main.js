01a501c2d1059b38898d963bedd313e6bd1172f0
tententententen <89884128+tententententen@users.noreply.github.com>
2021-09-15 11:15:28 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: '¥%〒~=:',
+          q1: '￥％〒〜＝',
         },
         stage2: {
           q1: '３８６８',
